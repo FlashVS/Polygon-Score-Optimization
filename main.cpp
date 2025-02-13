452677121ff532ba70a9687fbf1f687fe748712a
Vaibhav Singh <111372048+FlashVS@users.noreply.github.com>
2025-02-14 01:14:50 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,34 +19,25 @@ vector<node> crystal, mine;
 int achieved;
 int totalPositive;
 int totalNegative; 
-int best165points;
-int worst165points;
+int total;
+int total_achieved;
 
 void _input(){
 
     pts.clear();
+
     cin >> n;
     crystal.clear();
     crystal.resize(n);
 
     totalPositive = 0;
     totalNegative = 0;
-    vector<int> t;
 
     for(int i = 0; i < n; i++){
         cin >> crystal[i].x >> crystal[i].y >> crystal[i].val;
         totalPositive += crystal[i].val;
-        t.push_back(crystal[i].val);
-
         pts[{crystal[i].x, crystal[i].y}] += crystal[i].val;
     }
-
-    best165points = 0;
-
-    sort(rall(t));
-    for(int i = 0; i < min(166ll, (int)t.size()); i++) best165points += t[i];
-
-    t.clear();
 
     cin >> m; 
     mine.clear();
@@ -55,18 +46,9 @@ void _input(){
     for(int i = 0; i < m; i++){
         cin >> mine[i].x >> mine[i].y >> mine[i].val;
         mine[i].val *= -1;
-        t.push_back(mine[i].val);
         totalNegative += mine[i].val;
-
         pts[{mine[i].x, mine[i].y}] += mine[i].val;
     }
-
-    worst165points = totalPositive + totalNegative;
-
-    sort(all(t));
-    for(int i = 0; i < min(166ll, (int)t.size()); i++) worst165points -= t[i];
-
-    achieved = max(worst165points, best165points);
 }
 
 pair<int, set<pair<pair<double, double>, pair<double, double>>>> generate_stripperX(int threshold){
@@ -606,43 +588,67 @@ signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //change input file name and output file name accordingly
+    int start = 0, end = 19;
 
-    freopen("input/input09.txt", "r", stdin); // input
-    freopen("output/output09.txt", "w", stdout); // output
+    total = 0;
+    total_achieved = 0;
 
-    achieved = 0;
+    for(int i = start; i <= end; i++){
 
-    _input();   
+        string inp = "";
+        string out = "";
 
-    pair<int, set<pair<pair<double, double>, pair<double, double>>>> ans;
+        string num = to_string(i);
+        if(num.size() == 1) num = "0" + num;
 
-    for(int j = 0; j >= -45000; j-= 1000){
-        pair<int, set<pair<pair<double, double>, pair<double, double>>>> temp = generate_stripperX(j);
-        if(ans.first < temp.first){
-            ans = temp;
+        inp = "input/input" + num + ".txt";
+        out = "output/output" + num + ".txt";
+
+        freopen(inp.c_str(), "r", stdin); // input
+        freopen(out.c_str(), "w", stdout); // output
+
+        achieved = 0;
+
+        _input();   
+
+        pair<int, set<pair<pair<double, double>, pair<double, double>>>> ans = {0, {}};
+
+        for(int j = 0; j >= -45000; j-= 1000){
+            pair<int, set<pair<pair<double, double>, pair<double, double>>>> temp = generate_stripperX(j);
+            if(ans.first < temp.first){
+                ans = temp;
+            }
         }
-    }
 
-    for(int j = 0; j >= -45000; j -= 1000){
-        pair<int, set<pair<pair<double, double>, pair<double, double>>>> temp = generate_stripperY(j);
-        if(ans.first < temp.first){
-            ans = temp;
+        for(int j = 0; j >= -45000; j -= 1000){
+            pair<int, set<pair<pair<double, double>, pair<double, double>>>> temp = generate_stripperY(j);
+            if(ans.first < temp.first){
+                ans = temp;
+            }
         }
+
+        pair<int, set<pair<pair<double, double>, pair<double, double>>>> ans1 = generateBest165();
+        if(ans1.first >= ans.first) ans = ans1;
+
+        ans1 = generateWorst165();
+
+        if(ans1.first >= ans.first) ans = ans1;
+
+        cout << ans.first << endl;
+        cout << ans.first * 100.0 / totalPositive << "%" << endl;
+        cout << ans.second.size() << endl;
+        for(auto i : ans.second){
+            cout << "(" << i.first.first << ", " << i.first.second << "), (" << i.second.first << ", " << i.second.second << ")" << endl;
+        }
+
+        total_achieved += ans.first;
+        total += totalPositive;
     }
 
-    pair<int, set<pair<pair<double, double>, pair<double, double>>>> ans1 = generateBest165();
-    if(ans1.first >= ans.first) ans = ans1;
-
-    ans1 = generateWorst165();
-
-    if(ans1.first >= ans.first) ans = ans1;
-
-    cout << ans.first << endl;
-    cout << ans.second.size() << ", " << ans.second.size() << endl;
-    for(auto i : ans.second){
-        cout << "(" << i.first.first << ", " << i.first.second << "), (" << i.second.first << ", " << i.second.second << ")" << endl;
-    }
+    freopen("output/final_output.txt", "w", stdout);
+    cout << total << endl;
+    cout << total_achieved << endl;
+    cout << total_achieved * 100.0 / total << "%" << endl;
 
     return 0;
 }
